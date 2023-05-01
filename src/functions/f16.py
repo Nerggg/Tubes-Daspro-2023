3ad0bf494c965bf62c595b25f72842014d8476e0
Nerg <mas.ikhwanalhakim@gmail.com>
2023-05-01 16:44:01 +0700

    menambahkan komentar pada fungsi exit

--- src/functions/f16.py
+++ src/functions/f16.py
@@ -1,6 +1,17 @@
 from functions import f14
+
+# fungsi ini menerima matriks 'user' yang berisi data seluruh user,
+# matriks 'candi' yang berisi data candi yang telah dibangun,
+# matriks 'bahan' yang berisi bahan-bahan yang dimiliki pemain,
+# dan string 'address' yang berisi alamat lokasi file data game
 def exit(user, candi, bahan, address):
+
+    # bertanya kepada pemain apakah ingin menyimpan game atau tidak
     opt = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (Y/N) ")
+
+    # jika ingin menyimpan game maka
     if opt == 'y':
-        f14.save(user, candi, bahan, address)
+        f14.save(user, candi, bahan, address) # kita panggil fungsi save
+
+    # mengembalikan 'false' agar program berhenti
     return False
